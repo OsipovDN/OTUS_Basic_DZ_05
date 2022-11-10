ba80aeca3f063c0292839c022e40f0e44565af66
OsipovDN <disorder_2@list.ru>
2022-11-10 18:57:36 +0300

    Добавлен класс процентиля 90

--- Include/Pct90.h
+++ Include/Pct90.h
@@ -19,18 +19,3 @@ private:
 	std::vector <double> array;
 
 };
-
-void Pct90::update(double next) {
-	double i=0;
-	array.push_back(next);
-	sort(array.begin(), array.end());
-	i =ceil( 0.9 * array.size());
-	count = array[i];
-}
-
-double Pct90::eval()const {
-	return count;
-}
-const char* Pct90::name() const {
-	return "pct90";
-}
